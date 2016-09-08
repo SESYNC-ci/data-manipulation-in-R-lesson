544afefc423229d306fa85389bf13871812fb5b4
Ian Carroll <icarroll@sesync.org>
2016-09-08 09:23:36 -0400

    seems good accept for definition lists

--- build.R
+++ build.R
@@ -6,26 +6,30 @@ config = yaml.load_file("_config.yml")
 render_markdown(fence_char = "~")
 opts_knit$set(base.url = paste0(config$baseurl, "/"))
 opts_chunk$set(
-  comment = NA,
-  fig.path = "images/",
-  block_ial = c("{:.input}", "{:.output}"))
+    comment = NA,
+    fig.path = "images/",
+    block_ial = c("{:.input}", "{:.output}"))
 
 current_chunk = knit_hooks$get("chunk")
 chunk = function(x, options) {
-  x <- current_chunk(x, options)
-  x <- gsub("~~~\n\n",
-            paste0("~~~\n", options$block_ial[1], "\n\n"),
-            x)
-  if (str_count(x, "~~~") > 2) {
-    idx <- 2
-  } else {
-    idx <- 1
-  }
-  x <- gsub("~~~(\n*$)",
-            paste0("~~~\n", options$block_ial[idx], "\\1"),
-            x)
-  return(x)
+    x <- current_chunk(x, options)
+    x <- gsub("~~~\n\n",
+              paste0("~~~\n", options$block_ial[1], "\n\n"),
+              x)
+    if (str_count(x, "~~~") > 2) {
+        idx <- 2
+    } else {
+        idx <- 1
+    }
+    x <- gsub("~~~(\n*$)",
+              paste0("~~~\n", options$block_ial[idx], "\\1"),
+              x)
+    return(x)
 }
 knit_hooks$set(chunk = chunk)
 
-knit("index.Rmd")
+setwd("_slides")
+
+for (f in config$slide_sorter) {
+    knit(paste0(f, ".Rmd"))
+}
--- instructor/index.md
+++ instructor/index.md
@@ -1,8 +1,20 @@
 ---
-reveal: "https://cdn.rawgit.com/hakimel/reveal.js/3.3.0"
 layout: slideshow
+reveal: https://cdn.rawgit.com/hakimel/reveal.js/3.3.0
+reveal-theme: /css/theme/sky.css
+style: /css/slideshow.css
 ---
 
-{% for p in site.pages %}
-{% if p.url == '/' %}{{ p.content}}{% endif %}
+{% for sorted in site.slide_sorter %}
+{% assign id = "/slides/" | append: sorted %}
+{% assign hslide = site.slides | where: "id", id | first %}
+<section>
+{% assign vslides = hslide.content | split: "<!--split-->" %}
+{% assign split =  vslides | size %}
+{% for vslide in vslides %}
+<section{% if hslide.background %} data-background="{{ site.baseurl }}{{ hslide.background }}"{% endif %}{% if hslide.class %} class="{{ hslide.class }}"{% endif %}>
+{{ vslide }}
+</section>
+{% endfor %}
+</section>
 {% endfor %}
